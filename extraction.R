275ebb14ed7bdcf52e8fe593f47b04c41a3f1360
Izel <gpmeneses@gmail.com>
2019-08-06 23:51:24 +0100

    Adding the R project for data extraction and analysis

---
+++ extraction.R
@@ -0,0 +1,22 @@
+######################################
+# Data Extraction
+######################################
+
+library(openair)
+library(stringr)
+
+# Extracting data of Stations (Metadata) from AURN network
+UKStations = importMeta(source = "AURN", all=TRUE)
+
+# Obtaining the stations located in Greater London and save the info in a 
+# csv file
+GLStations = subset(UKStations, zone_id == "A01")
+write.csv(GLStations,paste("data/", "LdnMetaStations", ".csv", sep = "", collapse = NULL))
+
+# Obtaining the data from stations located in Greater London area since 2013 and 2018
+for(i in 2013:2018){
+  ldnPollutants = importAURN(site= stationsCode$code, pollutant= "all", year = i,
+                             hc = TRUE, meta = FALSE, verbose = FALSE)
+  write.csv(ldnPollutants,paste("data/", i, ".csv", sep = "", collapse = NULL))
+}
+
