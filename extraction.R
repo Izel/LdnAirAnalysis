668794a0395bda4f269748706481815295568c3f
Gloria Meneses <gpmeneses@gmail.com>
2021-04-15 16:57:49 +0200

    modifiying the year measure extraction

--- extraction.R
+++ extraction.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 ###############################################################################
 # Data Extraction
 # Extracting data of Stations (Metadata) from AURN network and its measures
@@ -5,26 +6,38 @@
 
 library(openair)
 library(stringr)
+library(dplyr)
 
 # Obtaining the stations in U.K
 UKStations = importMeta(source = "AURN", all=TRUE)
 
 # Obtaining the stations located in Greater London and save the info in a 
 # csv file.
-GLStations = subset(UKStations, zone_id == "A01")
+GLStations = subset(UKStations, grepl("London", UKStations$site))
+
 write.csv(GLStations,paste("data/", "LdnMetaStations", ".csv", sep = "",
                            collapse = NULL))
 
 # Obtaining the data from stations located in Greater London area since 2013 
-# and 2019. For this project, data from 2013-2018 will be used to train, test
-# and validate the model. Data for 2019 will be used to predictions after the
-# model construction
-for(i in 2013:2019){
-  ldnPollutants = importAURN(site= stationsCode$code, pollutant= "all", 
-                             year=i, hc=TRUE, meta=FALSE, verbose=FALSE)
-  write.csv(ldnPollutants,paste("data/", i, ".csv", sep = "", 
-                                collapse = NULL))
-}
+# and 2019. For this project, data from 2013-2018 
+
+stationsCode = distinct(GLStations)
+
+keep <- c("date","no2", "o3", "pm2.5", "pm25", "pm10", "ws", "wd", "site", 
+          "code")
+
+ldnPollutants = importAURN(site=stationsCode$code, pollutant= "all", 
+                             year=2013:2019, hc=TRUE, meta=FALSE, verbose=FALSE)
+  
+finalLdnPollutants = ldnPollutants[ , (names(ldnPollutants) %in% keep)]
+head(finalLdnPollutants)
+write.csv(finalLdnPollutants, paste("data/", "ldnAir_dataset", ".csv", 
+                                    sep = "", collapse = NULL))
+
+
+
+
+
 
 
 
